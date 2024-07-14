6ccc3acf661a228584ba40e01a2ade6a3f2af96b
Rafsan12 <rafsan.web.2022@gmail.com>
2024-07-14 22:09:57 +0600

    problem 7 completed

---
+++ index-8.js
@@ -0,0 +1,9 @@
+const students = [
+  { name: "Alice", grade: 90 },
+  { name: "Bob", grade: 85 },
+  { name: "Charlie", grade: 92 },
+];
+
+for (const student of students) {
+  console.log(`Name: ${student.name}, Grade: ${student.grade}`);
+}
